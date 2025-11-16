2fecd74090f5a8a43f9bca67b95649d60e9ec1ae
Bruce Wayne <admin@bige0.com>
2025-11-16 15:48:02 +0800

    test: fix tests

--- UnitTest/GitHubReleasesTest.cs
+++ UnitTest/GitHubReleasesTest.cs
@@ -1,9 +1,29 @@
+using System.Net;
+using System.Net.Http.Headers;
+
 namespace UnitTest;
 
 [TestClass]
 public class GitHubReleasesTest
 {
 	public TestContext TestContext { get; set; }
+
+	private static HttpClient _httpClient = null!;
+
+	[AssemblyInitialize]
+	public static void AssemblyInit(TestContext context)
+	{
+		_httpClient = new HttpClient(new SocketsHttpHandler { AutomaticDecompression = DecompressionMethods.Brotli }, true);
+		_httpClient.DefaultRequestVersion = HttpVersion.Version30;
+
+		_httpClient.DefaultRequestHeaders.UserAgent.Clear();
+		_httpClient.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0");
+
+		if (Environment.GetEnvironmentVariable("GITHUB_TOKEN") is { } token)
+		{
+			_httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+		}
+	}
 
 	[TestMethod]
 	public async Task VersionNotFoundTest()
@@ -16,7 +36,7 @@ public class GitHubReleasesTest
 			@"1.6.0",
 			tag => tag.Replace(@"v", string.Empty)
 		);
-		await Assert.ThrowsExactlyAsync<VersionNotFoundException>(async () => Assert.IsFalse(await updaterChecker.CheckAsync(TestContext.CancellationToken)));
+		await Assert.ThrowsExactlyAsync<VersionNotFoundException>(async () => Assert.IsFalse(await updaterChecker.CheckAsync(_httpClient, TestContext.CancellationToken)));
 	}
 
 	[TestMethod]
@@ -30,7 +50,7 @@ public class GitHubReleasesTest
 			@"1.6.0",
 			tag => tag.Replace(@"v", string.Empty)
 		);
-		Assert.IsFalse(await updaterChecker.CheckAsync(TestContext.CancellationToken));
+		Assert.IsFalse(await updaterChecker.CheckAsync(_httpClient, TestContext.CancellationToken));
 	}
 
 	[TestMethod]
@@ -44,7 +64,7 @@ public class GitHubReleasesTest
 			@"1.5.9",
 			tag => tag.Replace(@"v", string.Empty)
 		);
-		Assert.IsTrue(await updaterChecker.CheckAsync(TestContext.CancellationToken));
+		Assert.IsTrue(await updaterChecker.CheckAsync(_httpClient, TestContext.CancellationToken));
 	}
 
 	[TestMethod]
@@ -58,7 +78,7 @@ public class GitHubReleasesTest
 			@"1.6.0",
 			tag => tag.Replace(@"v", string.Empty).Replace(@"-steam", string.Empty)
 		);
-		Assert.IsFalse(await updaterChecker.CheckAsync(TestContext.CancellationToken));
+		Assert.IsFalse(await updaterChecker.CheckAsync(_httpClient, TestContext.CancellationToken));
 	}
 
 	[TestMethod]
@@ -72,6 +92,6 @@ public class GitHubReleasesTest
 			@"1.5.9",
 			tag => tag.Replace(@"v", string.Empty).Replace(@"-steam", string.Empty)
 		);
-		Assert.IsTrue(await updaterChecker.CheckAsync(TestContext.CancellationToken));
+		Assert.IsTrue(await updaterChecker.CheckAsync(_httpClient, TestContext.CancellationToken));
 	}
 }
